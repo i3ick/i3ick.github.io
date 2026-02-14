a8c3fa228402c4e4d6353d4fd5d502a98e450ea4
Karlo <soulcreature@hotmail.com>
2026-02-14 22:32:54 +0100

    Starting a new branch with new files

---
+++ shorts.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Kazun – Animator</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+<?php include "navbar.html"; ?>
+
+<main class="shortsContent">
+
+	<div class="shortsTitle">
+	<h1>Shorts</h1> </div>
+	<div class="shortsGrid">
+
+		<div class="shortContainer">
+		<div class="title">Aspirine</div>
+		<div class="longInfo"><p> Short Animation based on Joann Sfar's comic. </p></div>
+		<video autoplay="autoplay" controls="" muted="muted" loop="loop" width="100%"><source src="/videos/aspirine_crop.mp4" type="video/mp4"></video>
+		</video>
+		<div class="shortInfo"> 2D Animation, 3", 2020 </div>
+		</div>
+		
+		<div class="shortContainer">
+		<div class="title">MSW Promo</div>
+		<div class="longInfo"><p> Promo video for a personal project which is in development. </p></div>
+		
+		<div class="embed-responsive embed-responsive-16by9">
+			<iframe allowfullscreen class="embed-responsive-item" src=
+			"https://player.vimeo.com/video/344763716" style="width: 100%;/*! height: 100%; */aspect-ratio: 16/9;"></iframe>
+		</div>
+		
+		<div class="shortInfo"> 2D Animation, 3", 2020 </div>
+		</div>
+				
+		<div class="shortContainer">
+		<div class="title">DOGS</div>
+		<div class="longInfo"><p> Graduation short about a young Polish partisan fleeing from the Warsaw Uprising. </p></div>
+		<div class="embed-responsive embed-responsive-16by9">
+			<iframe allowfullscreen class="embed-responsive-item" src=
+			"https://www.youtube.com/embed/TUYq6dh1nI0" style="width: 100%;/*! height: 100%; */aspect-ratio: 16/9;"></iframe>
+		</div>
+		<div class="shortInfo"> 2D Animation, 3", 2020 </div>
+		</div>
+		
+				
+	
+	</div>
+
+</main>
+
+<?php include "footer.html"; ?>
+
+</body>
+</html>
